961e8b15edb001e9107ac7a3c8a685b72d3da8b3
Evan Sosenko <razorx@evansosenko.com>
2018-05-14 23:50:29 -0700

    Add tslint

--- assistant/index.ts
+++ assistant/index.ts
@@ -3,15 +3,15 @@ import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
 export const hello: Handler = (
   event: APIGatewayEvent,
   context: Context,
-  cb: Callback
+  cb: Callback,
 ) => {
   const response = {
-    statusCode: 200,
     body: JSON.stringify({
+      input: event,
       message:
         'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event
-    })
+    }),
+    statusCode: 200,
   }
 
   cb(null, response)
