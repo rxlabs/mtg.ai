1a9107161831266f94a8189b600145712521758e
Evan Sosenko <razorx@evansosenko.com>
2018-05-14 22:42:07 -0700

    Factor out graphql handler

--- gql/src/mtg_gql/core.clj
+++ gql/src/mtg_gql/core.clj
@@ -5,11 +5,15 @@
    [taoensso.timbre :as log]
    [clojure.java.io :as io]))
 
-(deflambdafn mtg-gql.core.Hello
-  [in out context]
-  (log/info "Starting Lambda")
+(defn graphql [body ctx] body)
+
+(defn handle-json
+  [f in out ctx]
   (let [body (-> in io/reader (json/parse-stream keyword))
-        result (assoc {} :data (-> body :max num))]
+        res (f body ctx)]
     (with-open [w (io/writer out)]
-      (json/generate-stream result w)
-      (log/info "Lambda finished"))))
+      (json/generate-stream res w))))
+
+(deflambdafn mtg-gql.core.GraphQL
+  [in out ctx]
+  (handle-json graphql in out ctx))
