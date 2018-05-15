c860298c7a477c78893dbe1b6aa4cddd3ab1e044
Evan Sosenko <razorx@evansosenko.com>
2018-05-15 00:33:27 -0700

    Remove optimization key in webpack config

--- assistant/webpack.config.js
+++ assistant/webpack.config.js
@@ -18,9 +18,6 @@ module.exports = {
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js'
   },
-  optimization: {
-    minimize: false
-  },
   target: 'node',
   mode: 'production',
   module: {
