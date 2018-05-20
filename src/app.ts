2cc09321eebf1a251c3be6123d4aee52602206e3
Evan Sosenko <razorx@evansosenko.com>
2018-05-19 22:35:04 -0700

    Add get_card_by_name intent

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import { dialogflow } from 'actions-on-google'
+import request from 'request'
+
+const req = request.defaults({
+  baseUrl: 'https://api.magicthegathering.io/v1',
+  json: true
+})
+
+const findCard = (name, cb) => {
+  req.get('/cards', { qs: { name } }, cb)
+}
 
 export function dialogflowApp() {
   const app = dialogflow()
 
-  app.intent('make_name', (conv, { color, num }) => {
-    conv.close(`Alright, your silly name is ${color} ${num}!`)
+  app.intent('get_card_by_name', (conv, { cardName }) => {
+    findCard(cardName, (err, res, body) => {
+      if (err || body.cards.length === 0) {
+        conv.close('Could not find card.')
+      }
+
+      const { name, text } = body.cards[0]
+      conv.close(`The card ${name} reads ${text}`)
+    })
   })
 
   return app
