18701b6275c8bf9a347b9ce423b10aaf269bd060
Evan Sosenko <razorx@evansosenko.com>
2018-05-19 21:18:46 -0700

    Use json and createServer

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import 'source-map-support/register'
 // tslint:disable-next-line:no-var-requires
 const serverless = require('serverless-http')
 
+import { createServer } from 'http'
+
 import * as bodyParser from 'body-parser'
 import * as connect from 'connect'
 import { dialogflowApp } from './src/app'
 
 const app = connect()
 
-app.use(bodyParser())
+app.use(bodyParser.json())
 app.use(dialogflowApp())
 
-export const fulfillment = serverless(app)
+module.exports.fulfillment = serverless(createServer(app))
