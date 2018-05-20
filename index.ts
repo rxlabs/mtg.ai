38f14305af490e4b703f6b17b6d177c096d644e7
Evan Sosenko <razorx@evansosenko.com>
2018-05-19 21:20:55 -0700

    Do not use createServer

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@ import 'source-map-support/register'
 // tslint:disable-next-line:no-var-requires
 const serverless = require('serverless-http')
 
-import { createServer } from 'http'
-
 import * as bodyParser from 'body-parser'
 import * as connect from 'connect'
 import { dialogflowApp } from './src/app'
@@ -14,4 +12,4 @@ const app = connect()
 app.use(bodyParser.json())
 app.use(dialogflowApp())
 
-module.exports.fulfillment = serverless(createServer(app))
+module.exports.fulfillment = serverless(app)
