0eae8cef9714a37b68f9aac810a2d3e8492d5025
Evan Sosenko <razorx@evansosenko.com>
2018-05-19 21:06:51 -0700

    Use lambda framework

--- index.ts
+++ index.ts
@@ -1,15 +1,5 @@
 import 'source-map-support/register'
 
-// tslint:disable-next-line:no-var-requires
-const serverless = require('serverless-http')
-
-import * as bodyParser from 'body-parser'
-import * as connect from 'connect'
 import { dialogflowApp } from './src/app'
 
-const app = connect()
-
-app.use(bodyParser())
-app.use(dialogflowApp())
-
-export const fulfillment = serverless(app)
+module.exports.fulfillment = dialogflowApp()
