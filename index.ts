2aef961eccae113a5f9329096cb10556564b0a7f
Evan Sosenko <razorx@evansosenko.com>
2018-05-19 21:07:11 -0700

    Revert "Use lambda framework"
    This reverts commit 0eae8cef9714a37b68f9aac810a2d3e8492d5025.

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 import 'source-map-support/register'
 
+// tslint:disable-next-line:no-var-requires
+const serverless = require('serverless-http')
+
+import * as bodyParser from 'body-parser'
+import * as connect from 'connect'
 import { dialogflowApp } from './src/app'
 
-module.exports.fulfillment = dialogflowApp()
+const app = connect()
+
+app.use(bodyParser())
+app.use(dialogflowApp())
+
+export const fulfillment = serverless(app)
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { dialogflow } from 'actions-on-google'
 
 export function dialogflowApp() {
-  const app = dialogflow()
+  const app = dialogflow({ debug: true })
 
   app.intent('make_name', (conv, { color, num }) => {
     conv.close(`Alright, your silly name is ${color} ${num}!`)
