549049e870a6c1b4a7cf858ce80ea8b15ce5f3f2
Evan Sosenko <razorx@evansosenko.com>
2018-05-20 18:51:51 -0700

    Add build script

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ const path = require('path')
 const serverless = require('serverless-webpack')
 const nodeExternals = require('webpack-node-externals')
 
+const isServerless = Object.keys(serverless.lib.entries).length !== 0
+
 module.exports = {
-  entry: serverless.lib.entries,
+  entry: isServerless
+    ? serverless.lib.entries
+    : path.join(__dirname, 'index.js'),
   target: 'node',
   mode: serverless.lib.webpack.isLocal ? 'development' : 'production',
   optimization: {
@@ -30,7 +34,9 @@ module.exports = {
   },
   output: {
     libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '.webpack'),
+    path: isServerless
+      ? path.join(__dirname, '.webpack')
+      : path.join(__dirname, 'build'),
     filename: '[name].js',
     sourceMapFilename: '[file].map'
   }
