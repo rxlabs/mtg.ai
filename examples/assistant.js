10312c1a0d2f14edacbbefc5777d6be4cec6f328
Evan Sosenko <razorx@evansosenko.com>
2018-12-23 11:52:09 -0600

    Update fixtures

--- examples/assistant.js
+++ examples/assistant.js
@@ -6,9 +6,10 @@ import { dialogflowApp } from '../lib'
 
 export default ({ log }) => async (file = 'card-name') => {
   const app = dialogflowApp()
-  const req = await readJson(path.join('fixtures', `post.json`))
+  const event = await readJson(path.join('fixtures', 'post.json'))
+  const context = await readJson(path.join('fixtures', 'context.json'))
   const body = await readJson(path.join('fixtures', `${file}.json`))
-  req.body = JSON.stringify(body)
-  const result = app(req, {})
+  event.body = JSON.stringify(body)
+  const result = await app(event, context)
   return result
 }
