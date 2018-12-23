b7fc49a4f06b7f16acc37c1c218a499ba0a45965
Evan Sosenko <razorx@evansosenko.com>
2018-12-23 12:08:33 -0600

    Fix example

--- examples/assistant.js
+++ examples/assistant.js
@@ -6,10 +6,8 @@ import { dialogflowApp } from '../lib'
 
 export default ({ log }) => async (file = 'card-name') => {
   const app = dialogflowApp()
-  const event = await readJson(path.join('fixtures', 'post.json'))
+  const event = await readJson(path.join('fixtures', `${file}.json`))
   const context = await readJson(path.join('fixtures', 'context.json'))
-  const body = await readJson(path.join('fixtures', `${file}.json`))
-  event.body = JSON.stringify(body)
   const result = await app(event, context)
   return result
 }
